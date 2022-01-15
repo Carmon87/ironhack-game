c44a28a9bf4e3aa2da90d33328ccd6cd39375863
Jose Antonio Carmona <jacarmonamartin@gmail.com>
2022-01-15 11:48:26 +0100

    limit player movement inside canvas

---
+++ src/player.js
@@ -0,0 +1,47 @@
+
+class Carmo {
+    constructor(x,y,radius,color) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+    }
+    
+    draw(){
+        if ((this.x >= 25 && this.x < 775) && (this.y >= 25 && this.y < 775)){
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
+            ctx.fillStyle = this.color
+            ctx.fill();
+        }
+        
+    }
+
+    // clear() {
+    //     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    // }
+}
+
+let x = canvas.width/2;
+let y = canvas.height/2;
+
+let canv = document.getElementById('canvas');
+
+canvas.addEventListener('mousemove', e => {
+    console.log({x:e.offsetX, y:e.offsetY})
+    player.x = e.offsetX;
+    player.y = e.offsetY
+});
+
+const player = new Carmo(x, y, 30, 'red');
+player.draw()
+
+function update (){
+    player.draw();
+    // player.clear();
+    requestAnimationFrame(update)
+}
+
+update()
+
+console.log(player)
